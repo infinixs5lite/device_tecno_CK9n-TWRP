4a96a6de7032809dee40dd671eb43f6fa5566955
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-17 15:43:08 +0100

    Update twrp_CK9n.mk

--- twrp_CK9n.mk
+++ twrp_CK9n.mk
@@ -1,6 +1,6 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from earth device
+# Inherit from CK9n device
 $(call inherit-product, device/tecno/CK9n/device.mk)
 
-# Inherit some common TWRP stuff.
+# Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := CK9n
